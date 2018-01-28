ff6876e27c74f88627eb196d0fd42f218846f7bf
Thomas Black <sheepoox@gmail.com>
2018-01-28 17:56:10 +0100

    Added matplotlibrc config file and both used and to be used sources. Minor code changes that do not affect functionality.

--- test.py
+++ test.py
@@ -1,11 +1,23 @@
-import matplotlib as mpl
+#import matplotlib as mpl
 from mpl_toolkits.mplot3d import axes3d
-import matplotlib.pyplot as plt
+import pylab as plt
+#import matplotlib.pyplot as plt
 import numpy as np
 
 import trajectory
 
-print("Backend in use: " + mpl.get_backend())
+#print(mpl.rcsetup.interactive_bk)
+#print(mpl.rcsetup.non_interactive_bk)
+#print(mpl.rcsetup.all_backends)
+
+#print("Backend in use: " + mpl.get_backend())
+
+def plot_to_plots(axis, data, label):
+    axis[0].plot(data[0], data[2], data[1], label=label)
+    axis[1].plot(data[0], data[1], label=label)
+    axis[2].plot(data[2], data[1], label=label)
+    axis[3].plot(data[0], data[2], label=label)
+
 
 fig = plt.figure()
 
@@ -13,6 +25,8 @@ ax1 = fig.add_subplot(221, projection='3d')
 ax2 = fig.add_subplot(222)
 ax3 = fig.add_subplot(223)
 ax4 = fig.add_subplot(224)
+
+axis = [ax1, ax2, ax3, ax4]
 
 ax1.set_title('3d view', loc='left')
 ax1.set_xlabel('x-axis', fontsize=10)
@@ -31,29 +45,14 @@ ax4.set_title('top view', loc='left')
 ax4.set_xlabel('x-axis', fontsize=10)
 ax4.set_ylabel('z-axis', fontsize=10)
 
-g = 9.81
-v0 = 30
-height = 10.47
-alpha = 45 * 3.14 / 180
-theta = 0
-
 t1 = trajectory.Trajectory(x=-8, y=5, z=5, h0=2)
 t1data = t1.calculate_trajectory()
 
 t2 = trajectory.Trajectory(x=10, y=5, z=10)
 t2data = t2.calculate_trajectory()
 
-ax1.plot(t1data[0], t1data[2], t1data[1], label='1')
-ax1.plot(t2data[0], t2data[2], t2data[1], label='2')
-ax1.legend()
-
-ax2.plot(t1data[0], t1data[1], label='1')
-ax3.plot(t1data[2], t1data[1], label='1')
-ax4.plot(t1data[0], t1data[2], label='1')
-
-ax2.plot(t2data[0], t2data[1], label='2')
-ax3.plot(t2data[2], t2data[1], label='2')
-ax4.plot(t2data[0], t2data[2], label='2')
+plot_to_plots(axis, t1data, '1')
+plot_to_plots(axis, t2data, '2')
 
 ax2.grid(color='grey', linestyle='--', linewidth='0.5')
 ax3.grid(color='grey', linestyle='--', linewidth='0.5')
